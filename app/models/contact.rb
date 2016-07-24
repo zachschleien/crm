8a3be923fe3c6053797f78a0d9b12186a2de2e36
arlyons <lyons500@gmail.com>
2016-07-24 09:54:17 -0400

    Disply false conversation history in contact show

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ApplicationRecord
   belongs_to :user
   has_many :conversations, dependent: :destroy
+
+  def filter_by_history(conversations)
+    @conversations = conversations.reject {|conversation| conversation.history == true }
+  end
+
 end
